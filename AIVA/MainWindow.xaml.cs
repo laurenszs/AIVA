2e0ead268f8b3df068b0f7afd60707daec23455e
Laurens <Laurenscnossen@gmail.com>
2024-05-09 18:02:30 +0200

    Add project files.

---
+++ AIVA/MainWindow.xaml.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace AIVA
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void RadioButton_Checked(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void CloseButton_Click(object sender, RoutedEventArgs e)
+        {
+            Application.Current.Shutdown();
+        }
+
+        private void CloseButton_OnMouseEnter(object sender, MouseEventArgs e)
+        {
+            CloseButton.Background = Brushes.Red;
+            CloseButton.Foreground = Brushes.Red;
+        }
+        private void CloseButton_OnMouseLeave(object sender, MouseEventArgs e)
+        {
+            CloseButton.Background = Brushes.Transparent;
+            CloseButton.Foreground = Brushes.White;
+        }
+    }
+}
